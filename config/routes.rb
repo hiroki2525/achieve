e9da2a0c4b266573fffb64fafd3c580a8f096757
hiroki2525 <peachcat.64274072@gmail.com>
2017-03-10 12:55:30 +0000

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,21 @@
+Rails.application.routes.draw do
+
+  get 'top/index'
+
+  get 'top/index'
+
+  resources :blogs, only: [:index, :new, :create, :edit, :update, :destroy] do
+    collection do
+      post :confirm
+    end
+  end
+  
+  resources :contacts, only: [:index, :new, :create] do
+    collection do
+      post :confirm
+    end
+  end
+
+  root 'top#index'
+  
+end
