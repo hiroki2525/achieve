8a90af5e80c2d0d8df1e69ca06e61af8b8c47307
hiroki2525 <peachcat.64274072@gmail.com>
2017-05-29 10:55:06 +0000

    シードデータheroku用変更

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,15 +7,17 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 100.times do |n|
+  name = Faker::Name.name
   email = Faker::Internet.email
   password = "password"
-  User.create!(email: email,
+  User.create!(name: name,
+    　　　　　　email: email,
             password: password,
             password_confirmation: password,
             )
 end
 
-n = 2
+n = 1
 while n <= 100
   Blog.create(
     title: "課題０９",
